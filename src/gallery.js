fe8ec157c307d2b4ece2210ec15f91387fbdd493
Bus42 <gbrewton1980@gmail.com>
2018-05-19 12:52:32 -0500

    Gets a slideshow for gallery

--- src/gallery.js
+++ src/gallery.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ImageGallery from 'react-image-gallery';
+import './gallery.css';
 
 //Image imports
 import img1 from './resources/1_small-min.jpeg';
@@ -21,17 +23,96 @@ import img17 from './resources/17_small-min.jpeg';
 import img18 from './resources/18_small-min.jpeg';
 //
 
-let images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18];
-
-const imageList = images.map((image) =>
-  <img className="galleryImg" src={image} alt="#" key={images.indexOf(image)} />
-);
-
 class Gallery extends Component {
     render() {
+
+        const galleryList = [
+            {
+                original: img1,
+                thumbnail: img1,
+            },
+            {
+                original: img2,
+                thumbnail: img2,
+            },
+            {
+                original: img3,
+                thumbnail: img3,
+            },
+            {
+                original: img4,
+                thumbnail: img4
+            },
+            {
+                original: img5,
+                thumbnail: img5
+            },
+            {
+                original: img6,
+                thumbnail: img6
+            },
+            {
+                original: img7,
+                thumbnail: img7
+            },
+            {
+                original: img8,
+                thumbnail: img8
+            },
+            {
+                original: img9,
+                thumbnail: img9
+            },
+            {
+                original: img10,
+                thumbnail: img10
+            },
+            {
+                original: img11,
+                thumbnail: img11
+            },
+            {
+                original: img12,
+                thumbnail: img12
+            },
+            {
+                original: img13,
+                thumbnail: img13
+            },
+            {
+                original: img14,
+                thumbnail: img14
+            },
+            {
+                original: img15,
+                thumbnail: img15
+            },
+            {
+                original: img16,
+                thumbnail: img16
+            },
+            {
+                original: img17,
+                thumbnail: img17
+            },
+            {
+                original: img18,
+                thumbnail: img18
+            }
+        ]
+
         return (
-            <div id="gallery">
-                {imageList}
+            <div className="flex-container">
+                <div id="gallery-wrapper">
+                    <ImageGallery
+                        originalClass='gallery-original'
+                        thumbnailClass='gallery-thumbnail'
+                        lazyLoad={true}
+                        items={galleryList}
+                        showThumbnails={false}
+                        autoPlay={true}
+                    />
+                </div>
             </div>
         );
     }
